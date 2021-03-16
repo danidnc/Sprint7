a38a3c441aa2ab93e2bdaf6dde34a53d9de65631
danidnc <danieldnc@outlook.es>
2021-03-16 16:26:27 +0100

    Nivel3 ejercicio 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from "react";
 
+
+import styled from "styled-components"
 import dataTexto from "./Texto.json"
 import Texto from "./Texto"
+import Boton from "./Botones"
+
+const ContenedorBotones = styled.div`
+    display:flex;
+    justify-content:space-between;
+    margin:2rem;
+`
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      texto: dataTexto
+      texto: dataTexto,
+      idLinea: 0
+    }
+    this.manejadorClickAnterior = this.manejadorClickAnterior.bind(this)
+    this.manejadorClickSiguiente = this.manejadorClickSiguiente.bind(this)
+  }
+
+  manejadorClickAnterior() {
+    if (this.state.idLinea > 0) {
+      this.setState({ idLinea: this.state.idLinea - 1 })
     }
   }
+
+  manejadorClickSiguiente() {
+    if (this.state.idLinea < (this.state.texto.length - 1)) {
+      this.setState({ idLinea: this.state.idLinea + 1 })
+    }
+  }
+
   render() {
-    const lineas = this.state.texto.map(linea => <Texto key={linea} linea={linea} />)
+    //const lineas = this.state.texto.map(linea => <Texto key={linea} linea={linea} />)
+    console.log(this.state.idLinea)
     return (
       <div>
-        {lineas}
+        <ContenedorBotones>
+          <Boton onClick={this.manejadorClickAnterior} title="Linea Anterior" />
+          <Boton onClick={this.manejadorClickSiguiente} title="Siguiente Linea" />
+        </ContenedorBotones>
+        <Texto linea={this.state.texto[this.state.idLinea]} />
       </div>
     )
   }
 
 }
+
 
 export default App;
